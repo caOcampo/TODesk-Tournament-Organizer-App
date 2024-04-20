571b900595d6d97e176dc18c2d5a0ecd547d903d
caOcampo <calvinmaster4@gmail.com>
2024-04-20 16:18:38 -0700

    Started layouts for double elimination, individual DElim matches

---
+++ app/src/main/java/com/example/todeskapp/ElimActivity.java
@@ -0,0 +1,38 @@
+package com.example.todeskapp;
+
+import android.os.Bundle;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.example.todeskapp.databinding.ActivityElimBinding;
+
+public class ElimActivity extends AppCompatActivity {
+
+private ActivityElimBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     binding = ActivityElimBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
+            }
+        });
+    }
+}
