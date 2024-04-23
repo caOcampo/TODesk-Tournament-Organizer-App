534af53dd0be940ca9a1bab6d9b107c338ef74c0
JawnChawn <JawnChawn@gmail.com>
2024-04-22 21:05:38 -0700

    add player java redirect & Player Profile java

---
+++ app/src/main/java/com/example/todeskapp/PlayerProfile.java
@@ -0,0 +1,36 @@
+package com.example.todeskapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todeskapp.databinding.PlayerProfileBinding;
+
+public class PlayerProfile extends AppCompatActivity{
+
+    private PlayerProfileBinding binding;
+    private String accessCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = PlayerProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        accessCode = getIntent().getStringExtra("ACCESS_CODE");
+
+
+        /* Setup redirection from PlayerProfile to AddPlayer */
+        binding.add.setOnClickListener(v -> {
+            accessCode = getIntent().getStringExtra("ACCESS_CODE");
+            Intent intent = new Intent(PlayerProfile.this, AddPlayer.class);
+            intent.putExtra("ACCESS_CODE", accessCode);  // Passing the access code to the next activity
+            startActivity(intent);
+        });
+
+
+    }
+}
+
